1374173f14e0ea89a24a0517d7e97139c8c10ab8
Joel Atyas <smilin@Dolphin-3.local>
2015-01-25 14:53:29 -0500

    Initial Commit

---
+++ Euler Problem 1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Euler Problem 1
+//
+//  Created by Joel Atyas on 1/25/15.
+//  Copyright (c) 2015 Joel Atyas. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
